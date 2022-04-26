d332b5bed49b855896bf32d804fa6457044e1322
Rafaelrm89 <Rafael.r.m89@gmail.com>
2022-04-26 17:02:16 +0200

    Add files via upload

---
+++ WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService weatherForecastService)
+        {
+            _logger = logger;
+            _weatherForecastService = weatherForecastService;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return _weatherForecastService.GetAllForecasts();
+        }
+
+        private IWeatherForecastService _weatherForecastService;
+    }
+}
